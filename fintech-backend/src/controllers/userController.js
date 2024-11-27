75538913ed9d586f9066c4a20bffb14129f17134
christinaduthie <christina@itoi.org>
2024-11-26 21:53:07 -0800

    rest

--- fintech-backend/src/controllers/userController.js
+++
@@ -1,5 +0,0 @@
-const getUsers = (req, res) => {
-    res.status(200).json({ message: 'User route working!' });
-};
-
-module.exports = { getUsers };
--- fintech-backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers } = require('../controllers/userController');
-
-// Route to get users
-router.get('/', getUsers);
-
-module.exports = router;
