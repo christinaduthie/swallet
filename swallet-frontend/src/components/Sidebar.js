d63f2a093b096375ab044d37c4b1db592de450eb
christinaduthie <christina@duthie.org>
2024-11-28 01:22:31 -0800

    Responsive UI Update

--- swallet-frontend/src/components/Sidebar.js
+++ swallet-frontend/src/components/Sidebar.js
@@ -11,28 +11,45 @@ const Sidebar = () => {
 
   const handleLogout = () => {
     logout();
+    setExpanded(false); // Close sidebar after logout
   };
 
   const toggleSidebar = () => {
     setExpanded(!expanded);
   };
 
+  const closeSidebar = () => {
+    setExpanded(false);
+  };
+
   return (
     <>
-      <Navbar expand="lg" variant="dark" className="d-lg-none" style={{ backgroundColor: '#542de8' }}>
-       
+      {/* Navbar for small screens */}
+      <Navbar
+        expand="lg"
+        variant="dark"
+        className="d-lg-none"
+        style={{ backgroundColor: '#542de8' }}
+      >
+        <Navbar.Brand as={Link} to="/" className="navbar-brand">
+          Swallet
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="sidebar-navbar-nav" onClick={toggleSidebar} />
       </Navbar>
 
+      {/* Overlay */}
+      {expanded && <div className="overlay" onClick={closeSidebar}></div>}
+
       <div className={`sidebar ${expanded ? 'active' : ''}`}>
         <Navbar.Brand as={Link} to="/" className="d-none d-lg-block navbar-brand">
-           Swallet
+          Swallet
         </Navbar.Brand>
         <Nav defaultActiveKey="/" className="flex-column">
           <Nav.Link
             as={Link}
             to="/"
             active={currentPath === '/'}
+            onClick={closeSidebar}
           >
             Home
           </Nav.Link>
@@ -40,19 +57,14 @@ const Sidebar = () => {
             as={Link}
             to="/add-funds"
             active={currentPath === '/add-funds'}
+            onClick={closeSidebar}
           >
             Add Funds
           </Nav.Link>
           {/* Add more navigation links as needed */}
         </Nav>
 
-       
-
-        <Button
-          variant="danger"
-          className="logout-button"
-          onClick={handleLogout}
-        >
+        <Button variant="danger" className="logout-button" onClick={handleLogout}>
           Logout
         </Button>
       </div>
